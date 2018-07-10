1fe5294bebe492cecd70ad443b39e172a396a0ae
Chandler Mayo <Chandler@ChandlerMayo.com>
2018-07-10 13:35:09 -0400

    Added text labels for uv index and welcome text. Set background color to green.

--- UVINFO/App.js
+++ UVINFO/App.js
@@ -1,29 +1,27 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
+ * UV Index
+ * https://github.com/chandler767/PubNub-UV-Index-Monitor
  *
- * @format
- * @flow
  */
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
 type Props = {};
 export default class App extends Component<Props> {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+         uvindex: 0
+      }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.welcome}>UV Index</Text>
+        <Text style={styles.uvindex}>{this.state.uvindex}</Text>
       </View>
     );
   }
@@ -34,16 +32,17 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#90EE90',
   },
   welcome: {
-    fontSize: 20,
+    fontSize: 45,
     textAlign: 'center',
     margin: 10,
+    paddingTop: 25,
   },
-  instructions: {
+  uvindex: {
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    fontSize: 60,
+    marginBottom: 15,
   },
 });
