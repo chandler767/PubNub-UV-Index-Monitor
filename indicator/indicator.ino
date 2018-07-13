3387497cf032bfc944bd88035e61a3010fdc3601
Chandler Mayo <Chandler@ChandlerMayo.com>
2018-07-13 13:32:28 -0400

    Cleanup.

--- indicator/indicator.ino
+++ indicator/indicator.ino
@@ -10,7 +10,7 @@ int rled = 14; // The PWM pins the LED is attached to.
 int gled = 12;
 int bled = 15;
 
-int uvindexvalue = 0;
+int uvindex = 0;
 
 void setup() {
     pinMode(rled, OUTPUT);
@@ -37,66 +37,63 @@ void loop() {
         delay(1000);
         return;
     }
-    String uvindex;
+    String message;
     while (pclient->wait_for_data()) {
         char c = pclient->read();
         //Serial.print(c);
-        uvindex = uvindex+String(c); // Append to string.
+        message = message+String(c); // Append to string.
     }
     pclient->stop();
     
     /*
     // Uncomment to get UV index value by parsing JSON using v5 of the ArduinoJSON library.
-    uvindex.remove(0, 1); // Remove extra bracket before JSON.
-    uvindex = uvindex.substring(0, uvindex.length() - 2); // Remove extra brackets after JSON.
+    message = message.substring(1, message.length() - 2); // Remove extra bracket before and extra brackets after JSON.
     StaticJsonBuffer<200> jsonBuffer;
-    JsonObject& root = jsonBuffer.parseObject(uvindex);
+    JsonObject& root = jsonBuffer.parseObject(message);
     if (!root.success()) { // Test if parsing succeeds.
       Serial.println("parseObject() failed");
     }
     else {
-      uvindexvalue = root["eon"]["uvindex"];
+      uvindex = root["eon"]["uvindex"];
     }
-    */
+    //*/
 
-    //* // Comment to parse JSON for UV index value.
+    // Comment to parse JSON for UV index value.
     // Get UV index value without parsing JSON. Only one value is needed and the location of the value is known in the message. No need for the ArduinoJSON library. 
-    if(uvindex.indexOf("uvindex") > 0) { // Ensure the message contains the uvindex (prevents a unexpected message from turning off the led).
-      uvindex.remove(0, 19); // Remove characters before value.
-      uvindex = uvindex.substring(0, uvindex.length() - 4); // Remove characters after value.
-      uvindexvalue = uvindex.toInt();
+    if(message.indexOf("uvindex") > 0) { // Ensure the message contains the UV iindex (prevents a unexpected message from turning off the led).
+      uvindex = message.substring(19, message.length() - 4).toInt(); // Remove characters before and after value.
     };
     //*/
 
     // Set the color of the led based on the UV index.
-    if ((uvindexvalue >= 0)  && (uvindexvalue < 1)) { // None 0 Off
+    if ((uvindex >= 0)  && (uvindex < 1)) { // None 0 Off
         analogWrite(rled, 0);
         analogWrite(gled, 0);
         analogWrite(bled, 0);
-    } else if ((uvindexvalue >= 1) && (uvindexvalue < 3)) { // Low 1-2 Green
+    } else if ((uvindex >= 1) && (uvindex < 3)) { // Low 1-2 Green
         analogWrite(rled, 0);
         analogWrite(gled, 255);
         analogWrite(bled, 0);
-    } else if ((uvindexvalue >= 3) && (uvindexvalue < 6)) { // Moderate 3-5 Yellow
+    } else if ((uvindex >= 3) && (uvindex < 6)) { // Moderate 3-5 Yellow
         analogWrite(rled, 255);
         analogWrite(gled, 255);
         analogWrite(bled, 0);
-    } else if ((uvindexvalue >= 6) && (uvindexvalue < 8)) { // High 6-7 Orange
+    } else if ((uvindex >= 6) && (uvindex < 8)) { // High 6-7 Orange
         analogWrite(rled, 255);
         analogWrite(gled, 120);
         analogWrite(bled, 0);
-    } else if ((uvindexvalue >= 8) && (uvindexvalue < 11)) { // Very High 8-10 Red
+    } else if ((uvindex >= 8) && (uvindex < 11)) { // Very High 8-10 Red
         analogWrite(rled, 255);
         analogWrite(gled, 0);
         analogWrite(bled, 0);
-    } else if (uvindexvalue >= 11) { // Extreme 11+ Purple
+    } else if (uvindex >= 11) { // Extreme 11+ Purple
         analogWrite(rled, 85);
         analogWrite(gled, 0);
         analogWrite(bled, 85);
     };
 
     Serial.print("UV Index: ");
-    Serial.print(uvindexvalue);
+    Serial.print(uvindex);
     Serial.println();
     delay(5000);
 }
