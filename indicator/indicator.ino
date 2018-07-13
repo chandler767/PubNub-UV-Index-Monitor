ea13666e9818cb485cc676e2d78a910b48776a48
Chandler Mayo <Chandler@ChandlerMayo.com>
2018-07-12 23:26:17 -0400

    Subscribing to receive UV index values from PubNub.

--- indicator/indicator.ino
+++ indicator/indicator.ino
@@ -4,8 +4,17 @@
  
 const static char ssid[] = "Sensor Network";
 const static char pass[] = "sens0rpassw0rd"; 
- 
+
+int rled = 14; // The PWM pins the LED is attached to.
+int gled = 12;
+int bled = 15;
+
+
 void setup() {
+    pinMode(rled, OUTPUT);
+    pinMode(gled, OUTPUT);
+    pinMode(bled, OUTPUT);
+
     Serial.begin(9600);
     WiFi.begin(ssid, pass);
     if(WiFi.waitForConnectResult() == WL_CONNECTED){
@@ -18,22 +27,7 @@ void setup() {
  
 void loop() {
     PubNub_BASE_CLIENT *client;
- 
-    Serial.println("publishing a message");
-    client = PubNub.publish("uvindex", "\{\"eon\":\{\"uvindex\":10\}\}");
-    if (!client) {
-        Serial.println("publishing error");
-        delay(1000);
-        return;
-    }
-    while (client->connected()) {
-        while (client->connected() && !client->available());
-        char c = client->read();
-        Serial.print(c);
-    }
-    client->stop();
-    Serial.println();
- 
+    
     Serial.println("waiting for a message (subscribe)");
     PubSubClient *pclient = PubNub.subscribe("uvindex");
     if (!pclient) {
@@ -41,11 +35,20 @@ void loop() {
         delay(1000);
         return;
     }
+    String uvindex;
     while (pclient->wait_for_data()) {
         char c = pclient->read();
-        Serial.print(c);
+        //Serial.print(c);
+        uvindex = uvindex+String(c);
     }
     pclient->stop();
+
+    // Alternatively parse the JSON, however only one value is needed and the location of the value is known in the string. No need for another package. 
+    uvindex.remove(0, 19); // Remove string before value.
+    uvindex = uvindex.substring(0, uvindex.length() - 4); // Remove string after value.
+    Serial.print("UV Index:"+uvindex);
+    int uvindexvalue = uvindex.toInt();
+
     Serial.println();
     delay(1000);
 }
