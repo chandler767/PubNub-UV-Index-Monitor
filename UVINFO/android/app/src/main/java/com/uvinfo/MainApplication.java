ccd898a555b5a2cf3eeae1792a9e59e031cd5dae
Chandler Mayo <Chandler@ChandlerMayo.com>
2018-07-19 23:11:47 -0400

    Linked Firebase Cloud Messages for push notifications with Android.

--- UVINFO/android/app/src/main/java/com/uvinfo/MainApplication.java
+++ UVINFO/android/app/src/main/java/com/uvinfo/MainApplication.java
@@ -3,6 +3,9 @@ package com.uvinfo;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
+import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +25,10 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+          new RNFirebasePackage(),
+          new RNFirebaseMessagingPackage(),
+          new RNFirebaseNotificationsPackage()
       );
     }
 
